24d4bf149ee2580a0836e13d4e6afa3b2db34074
Abdulrahman-salem <abdalrhman2000salem@gmail.com>
2023-05-27 15:23:51 +0200

    Task3

--- index.js
+++ index.js
@@ -39,3 +39,57 @@ function handelBtnTask1(e) {
 }
 
 btn.addEventListener("click", handelBtnTask1);
+
+//////////////////////////////////////////////////////////////////
+
+// Task 3
+let countryName;
+const DataURL = "https://ipapi.co/json/";
+const btnGetCountryName = document.querySelector(".getCountryName");
+
+const getCountryName = async (url) => {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        return response.json();
+    }
+    return response;
+    //
+};
+
+function storCountryNameInSessionStorage() {
+    return sessionStorage.setItem("country Name", atob(countryName));
+}
+
+function showCountryName() {
+    const sectionTask3 = document.querySelectorAll(".section-task3");
+
+    // before first Child of the section-task3 create and add the new child to show the country name
+    return sectionTask3[0].firstElementChild.before(
+        document.body.appendChild(
+            Object.assign(document.createElement("article"), {
+                innerHTML: `<h2> Country: </h2> <p class="countryName"> ${countryName}</p>`,
+            })
+        )
+    );
+}
+
+function handelBtnTask3(e) {
+    if (document.querySelector(".countryName")) {
+        return alert("City name already exists");
+    }
+    getCountryName(DataURL)
+        .then((data) => (countryName = data.country_name.valueOf()))
+        .catch((error) => console.log(error));
+
+    setTimeout(() => {
+        if (countryName) {
+            storCountryNameInSessionStorage();
+            showCountryName();
+        }
+    }, 500);
+}
+
+btnGetCountryName.addEventListener("click", handelBtnTask3);
+
+//////////////////////////////////////////////////////////////////
